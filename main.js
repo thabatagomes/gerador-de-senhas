d57c3114ffcc030023e9f10dda92c8af8e347a56
thabatagomes <159185229+thabatagomes@users.noreply.github.com>
2025-08-05 11:20:42 -0300

    05-08

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
-let tamanhoSenha = 5; 
+let tamanhoSenha = 12; 
 numeroSenha.textContent = tamanhoSenha;
-
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
+const simbolos = '!#*?%@';
 const botoes = document .querySelectorAll('.parametro-senha__botao');
-
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
+const forcaSenha = document.querySelector('.forca');
+ 
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
 
@@ -28,16 +34,45 @@ function aumentaTamanho(){
     geraSenha();
 }
 
-const campoSenha = document.querySelector('#campo-senha');
-const letraMaiusculas ='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+ 
+for (i = 0; i < checkbox.length; i++){
+    checkbox[i].onclick = geraSenha; 
+}
 geraSenha();
 
+
 function geraSenha(){
-    let senha = ";
+    let alfabeto = '';
+    if (checkbox[0].checked){
+        alfabeto = alfabeto + letrasMaiusculas;
+    }
+
+    if (checkbox[1].checked){
+        alfabeto = alfabeto + letrasMinusculas;
+    }
+
+    if (checkbox[2].checked){
+        alfabeto = alfabeto + letrasMaiusculas;
+            alfabeto = alfabeto + numeros;
+        }
+
+        if (checkbox[3].checked){
+            alfabeto = alfabeto + letrasMaiusculas;
+                alfabeto = alfabeto + simbolos;
+            }
+    
+
+
+    let senha = '';
     for( let i = 0; i < tamanhoSenha; i++){
-        let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+        let numeroAleatorio = Math.random()*alfabeto.length;
         numeroAleatorio = Math.floor(numeroAleatorio);
-        senha = senha + letrasMaiusculas(numeroAleatorio);
+        senha = senha + alfabeto(numeroAleatorio);
+
+        campoSenha.value = senha;
+        claificaSenha();
+
  }
 
  campoSenha.value = senha;
